9cfec7b532dc23a2663a1ba29b0f35434dacda6f
Victor Moura <vicmourator@gmail.com.br>
2025-05-30 13:17:35 -0300

    feature(models): Criando as models necessárias para o sistema (#5)

--- backend/app/Models/Movie.php
+++ backend/app/Models/Movie.php
@@ -1,4 +1,26 @@
-<?php 
+<?php
 
+namespace App\Models;
 
->
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $fillable = [
+        'tmdb_id',
+        'title',
+        'overview',
+        'poster_path',
+        'release_date',
+    ];
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class, 'movie_genre');
+    }
+
+    public function favoritedByUsers()
+    {
+        return $this->belongsToMany(User::class, 'favorite_movies')->withTimestamps();
+    }
+}
