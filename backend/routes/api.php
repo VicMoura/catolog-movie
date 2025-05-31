3f91daa9861b5bc2f583eb00880259c7d6ae2d54
Victor Moura <vicmourator@gmail.com.br>
2025-05-31 16:04:56 -0300

    refactor(api): Organizando as chamadas da api (#11)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,13 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TmdbController;
+use App\Http\Controllers\FavoritesMoviesController;
 
-Route::post('/register', [AuthController::class, 'register'])->withoutMiddleware('auth:sanctum');
-Route::post('/login', [AuthController::class, 'login']);
+/**
+ * Rotas públicas
+ */
+Route::post('/register', [UserController::class, 'create']);
+Route::post('/login', [UserController::class, 'login']);
 
+/**
+ * Rotas protegidas (autenticadas)
+ */
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+    
+    Route::get('/detail', [UserController::class, 'detail']);
+    Route::post('/logout', [UserController::class, 'logout']);
 
+    Route::get('/tmdb/search', [TmdbController::class, 'search']);
+
+    Route::prefix('favorites')->group(function () {
+        Route::post('/', [FavoritesMoviesController::class, 'create']);
+        Route::get('/', [FavoritesMoviesController::class, 'list']);
+        Route::delete('/{movie}', [FavoritesMoviesController::class, 'delete']);
+    });
+});
