b0144bccdb1993873297e88afe01c789603db0a6
Victor Moura <vicmourator@gmail.com.br>
2025-06-02 10:43:12 -0300

    fim

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,12 +1,1 @@
-until mysql -h mysql -uuser -puser -e "SELECT 1" &> /dev/null
-do
-  echo "Esperando o banco de dados iniciar..."
-  sleep 3
-done
-
-echo "Banco de dados disponível! Rodando migrations..."
-
-php artisan migrate --force
-
-# Depois sobe o servidor Laravel
-php artisan serve --host=0.0.0.0 --port=8000
+php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
