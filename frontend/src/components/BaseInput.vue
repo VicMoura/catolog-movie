c4a6506f6439221cbee3b92724b8d7609705f1c2
Victor Moura <vicmourator@gmail.com.br>
2025-06-01 21:08:13 -0300

    feature(components) : Input controlado com props: id, label, type, modelValue, placeholder, required (#13)

---
+++ frontend/src/components/BaseInput.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <label :for="id" class="block text-sm font-medium text-gray-300 mb-1">
+            {{ label }}
+        </label>
+        <input :id="id" :type="type" :value="modelValue" :placeholder="placeholder" :required="required" class="w-full px-4 py-3 border border-gray-600 bg-gray-700 text-white rounded-md
+             focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+            @input="$emit('update:modelValue', $event.target.value)" />
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    id: String,
+    label: String,
+    type: { type: String, default: 'text' },
+    modelValue: String,
+    placeholder: String,
+    required: Boolean,
+})
+</script>
