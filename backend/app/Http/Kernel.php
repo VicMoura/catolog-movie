9069e1d1662aaf0b4fcff77373ade5bfa550fa31
Victor Moura <vicmourator@gmail.com.br>
2025-06-01 21:48:56 -0300

    feature(cors) : Middleware global HandleCors adicionado na pilha global (#17)

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -6,20 +6,21 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
+    /**
+     * The application's global HTTP middleware stack.
+     */
+    protected $middleware = [
+        \Illuminate\Http\Middleware\HandleCors::class
+    ];
 
     /**
      * The application's route middleware groups.
-     *
-     * @var array<string, array<int, class-string|string>>
      */
     protected $middlewareGroups = [
-
         'api' => [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ]
+        ],
     ];
-
-    
 }
