2df9686cb8643a0cb0552b226b4219e3913622c0
Victor Moura <vicmourator@gmail.com.br>
2025-06-01 21:10:57 -0300

    feature(package) : Adicionando bibliotecas necessarias para o desenvolvimento (#13)

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',        // se você tiver
+    './src/**/*.{vue,js,ts,jsx,tsx}',  // aqui apontando para suas pastas de código
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
