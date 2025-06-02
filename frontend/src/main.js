4d098cd328b90f0ee7e828e04e69c77efe6df9bb
Victor Moura <vicmourator@gmail.com.br>
2025-06-01 21:17:59 -0300

    feature(index) : Integrar Pinia e plugins (persistência de estado) na aplicação Vue 3 (#15)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,20 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import "./index.css";
+import Vue3Toastify from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
+app.use(router);
+app.use(Vue3Toastify, {
+  autoClose: 1600,
+  position: "top-center",
+  theme: "light",
+});
+app.mount("#app");
