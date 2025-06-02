d2655cb72e6601bd48eefd7258a70df1fdd5be44
Victor Moura <vicmourator@gmail.com.br>
2025-06-01 22:13:31 -0300

    feature(factory) : Adicionando factory (#23)

--- backend/app/Models/FavoriteMovie.php
+++ backend/app/Models/FavoriteMovie.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class FavoriteMovie extends Pivot
 {
+    use HasFactory;
+
     protected $table = 'favorite_movies';
 
     protected $fillable = [
